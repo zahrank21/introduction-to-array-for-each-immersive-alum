be3a3c2d6841e3442dba2fa40cfdbdd70b6a8231
Zahran Kabir <zahrank@gmail.com>
2019-10-10 14:23:33 -0700

    stash

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(arr, callback){
+  arr.forEach(element => {
+    return callback(element)
+  });
+}
 
 // Add your changeCompletely() function here:
